9f922cb4f5a11ddcf812c096d6ab186da91e3347
Jaemin Choi <jchoi157@illinois.edu>
2020-01-12 23:08:51 -0500

    Update Summit benchmarking script

--- cuda/src/scripts/summit/bench.sh
+++ cuda/src/scripts/summit/bench.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #BSUB -P csc357
-#BSUB -W 0:10
+#BSUB -W 0:30
 #BSUB -nnodes 171
 #BSUB -J minife-n1024
 
 date
 
-cd $MEMBERWORK/csc357/miniFE/cuda/src
+cd $MEMBERWORK/csc357/hpm/apps/miniFE/cuda/src
 
 ranks=1024
 
 for iter in 1 2 3
 do
   echo "Running iteration $iter"
-  jsrun -n $ranks -a 1 -c 1 -g 1 ./miniFE-b -nx 3200 -ny 1600 -nz 1600 --num_devices 1 > miniFE-n"$ranks"-"$iter".out
+  jsrun -n $ranks -a 1 -c 1 -g 1 ./miniFE-b -nx 3200 -ny 1600 -nz 1600 --num_devices 1 > n"$ranks"-"$iter".out
 done
