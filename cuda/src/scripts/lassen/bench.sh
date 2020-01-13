5f44cc62534e72ef902dc86e59a770c5cee8e856
Jaemin Choi <jchoi157@illinois.edu>
2020-01-12 20:08:25 -0800

    Update Lassen benchmarking script

--- cuda/src/scripts/lassen/bench.sh
+++ cuda/src/scripts/lassen/bench.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #BSUB -G asccasc
-#BSUB -W 10
+#BSUB -W 30
 #BSUB -core_isolation 2
 #BSUB -q pbatch
-#BSUB -nnodes 1
-#BSUB -J minife-n4
+#BSUB -nnodes 256
+#BSUB -J minife-n1024
 
 date
 
-cd /g/g90/choi18/miniFE/cuda/src
+cd /g/g90/choi18/hpm/apps/miniFE/cuda/src
 
-ranks=4
+ranks=1024
 
 for iter in 1 2 3
 do
   echo "Running iteration $iter"
-  jsrun -n $ranks -a 1 -c 1 -g 1 -K 2 -r 4 ./miniFE-b -nx 400 -ny 400 -nz 200 --num_devices 1 > miniFE-n"$ranks"-"$iter".out
+  jsrun -n $ranks -a 1 -c 1 -g 1 -K 2 -r 4 ./miniFE-b -nx 3200 -ny 1600 -nz 1600 --num_devices 1 > n"$ranks"-"$iter".out
 done
